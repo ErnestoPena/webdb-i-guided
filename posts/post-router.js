e6299dff15c98f30bc16ac73a22ddd539ef81c23
Ernesto Pena <ernestopenad@gmail.com>
2019-08-07 07:41:59 -0500

    Knex guided project completed

--- posts/post-router.js
+++ posts/post-router.js
@@ -1,28 +1,71 @@
 const express = require('express');
-
+const dbconfig = require('../data/db-config')
 // database access using knex
 const db = require('../data/db-config.js');
 
 const router = express.Router();
 
-router.get('/', (req, res) => {
-
+router.get('/', async (req, res) => {
+    try {        
+        const posts = await db('posts').toSQL().toNative();
+        console.log(posts)
+    }
+    catch (err) {
+        res.status(500).json({message: err})
+    }
+    
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
+  const id = req.params.id;
 
+  try {
+    const post = await db('posts').where('id', '=' , id);
+    res.status(200).json(post);
+  }
+  catch (err) {
+    res.status(500).json({message: err});
+  }
 });
 
-router.post('/', (req, res) => {
-
+router.post('/', async (req, res) => {
+    const postData = req.body;
+    try {
+        const post = await db('posts').insert(postData);
+        res.status(201).json(post);
+    }
+    catch (err) {
+        res.status(500).status({message: "Could not add your post"});
+    }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
+    const {id} = req.params.id;
+    const changes = req.body;
 
+    try {
+        const count = await db('posts').where('id', '=', id).update(changes);
+    }
+    catch (err) {
+
+    }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
+    const {id} = req.params.id;
+    try {
+        const count = await db('posts').where({id}).del();
 
+        if (count) {
+            res.status(200).json({deleted: count});
+        } else {
+            res.status(404).json({message: 'could not find id'});
+        }
+    }
+
+    catch (err) {
+        res.status(500).json({message: ''})
+    }
 });
 
 module.exports = router;
